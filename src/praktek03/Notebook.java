48e7fd55c99eee630b3f875b484c1ff2fc0f4088
GalihPamungkas07 <32285690+GalihPamungkas07@users.noreply.github.com>
2018-04-07 16:01:16 +0700

    Buat method dan cetakInfo()

--- src/praktek03/Notebook.java
+++ src/praktek03/Notebook.java
@@ -6,4 +6,10 @@ public class Notebook {
     String merk;
     String spek;
     double harga;
+    
+    void cetakInfo(){
+        System.out.println( "Merk \t: "+merk+"\n"+
+                            "Spek \t: "+spek+"\n"+
+                            "Harga\t: "+harga);
+    }
 }
