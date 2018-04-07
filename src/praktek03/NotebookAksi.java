d92a34e1a3a57499644f8418c78803f96961f4c4
GalihPamungkas07 <32285690+GalihPamungkas07@users.noreply.github.com>
2018-04-07 16:30:31 +0700

    Menggunakan Method Set

--- src/praktek03/NotebookAksi.java
+++ src/praktek03/NotebookAksi.java
@@ -3,9 +3,10 @@ package praktek03;
 public class NotebookAksi {
     public static void main(String[] args) {
         Notebook n = new Notebook();
-        n.merk="Acer";
-        n.spek="2GB RAM, 500GB HDD";
-        n.harga=4000000;
+        
+        n.setMerk("Acer");
+        n.setSpek("2GB RAM, 500GB HDD");
+        n.setHarga(4000000);
         
         n.cetakInfo();
     }
