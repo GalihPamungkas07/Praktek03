5bda27d504fed72964509fb98a67804f33ffd050
GalihPamungkas07 <32285690+GalihPamungkas07@users.noreply.github.com>
2018-04-07 16:25:16 +0700

    Buat Getter and Setter

--- src/praktek03/Notebook.java
+++ src/praktek03/Notebook.java
@@ -12,4 +12,30 @@ public class Notebook {
                             "Spek \t: "+spek+"\n"+
                             "Harga\t: "+harga);
     }
+
+    public String getMerk() {
+        return merk;
+    }
+
+    public void setMerk(String merk) {
+        this.merk = merk;
+    }
+
+    public String getSpek() {
+        return spek;
+    }
+
+    public void setSpek(String spek) {
+        this.spek = spek;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+    
+    
 }
