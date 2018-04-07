5429720c09087f16d8036d2a9d5e71df22ade18d
GalihPamungkas07 <32285690+GalihPamungkas07@users.noreply.github.com>
2018-04-07 16:19:00 +0700

    Instance, Buat Atribut dan Panggil Method

--- src/praktek03/NotebookAksi.java
+++ src/praktek03/NotebookAksi.java
@@ -2,6 +2,11 @@ package praktek03;
 
 public class NotebookAksi {
     public static void main(String[] args) {
+        Notebook n = new Notebook();
+        n.merk="Acer";
+        n.spek="2GB RAM, 500GB HDD";
+        n.harga=4000000;
         
+        n.cetakInfo();
     }
 }
